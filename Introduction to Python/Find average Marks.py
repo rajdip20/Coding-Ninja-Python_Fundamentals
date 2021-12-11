59b03ff64a6544e32641ac7e1472530d9a8c3b3b
rajdip20 <dasrajdip78754282@gmail.com>
2021-12-12 01:12:32 +0530

    Initial commit

---
+++ Introduction to Python/Find average Marks.py
@@ -0,0 +1,10 @@
+# Write a program to input marks of three tests of a student (all integers). 
+# Then calculate and print the average of all test marks.
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+d = a+b+c
+d = d/3
+print(d)
